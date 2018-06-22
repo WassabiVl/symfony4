8f8eebe0ec97820c2267925de76347fc9bc04110
waelatrash <wa@lombego.de>
2018-06-22 17:31:07 +0200

    inital 2nd phase

--- config/bundles.php
+++ config/bundles.php
@@ -24,4 +24,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
